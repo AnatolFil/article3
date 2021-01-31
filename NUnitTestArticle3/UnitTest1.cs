a23a291d3cc0211dcee4e30fefd49e047cde89ac
AnatolFil <tolyanfilatov@mail.ru>
2021-01-31 23:04:43 +0700

    added function pop() for myStack. Added class myFIFO

--- NUnitTestArticle3/UnitTest1.cs
+++ NUnitTestArticle3/UnitTest1.cs
@@ -36,5 +36,45 @@ namespace NUnitTestArticle3
             }
             Assert.AreEqual(lenght, stack.Count);
         }
+        [Test]
+        public void TestPopForMyStack()
+        {
+            int lenght = 100000000;
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < lenght; i++)
+            {
+                stack.Push(i);
+            }
+            for(int i = lenght-1; i>=0;i--)
+            {
+                Assert.AreEqual(i, stack.Pop());
+            }
+        }
+        [Test]
+        public void TestAddForMyFIFO()
+        {
+            Random rand = new Random(DateTime.Now.Millisecond);
+            int lenght = 100000000;
+            myFIFO<int> fifo = new myFIFO<int>();
+            for (int i = 0; i < lenght; i++)
+            {
+                fifo.add(rand.Next());
+            }
+            Assert.AreEqual(lenght, fifo.Lenght);
+        }
+        [Test]
+        public void TestRemoveForMyFIFO()
+        {
+            int lenght = 100000000;
+            myFIFO<int> fifo = new myFIFO<int>();
+            for (int i = 0; i < lenght; i++)
+            {
+                fifo.add(i);
+            }
+            for (int i = 0; i > lenght; i++)
+            {
+                Assert.AreEqual(i, fifo.remove());
+            }
+        }
     }
 }
--- article3/Program.cs
+++ article3/Program.cs
@@ -6,13 +6,14 @@ namespace article3
     {
         static void Main(string[] args)
         {
-            
+
         }
     }
     public class stackElement<T>
     {
         public T value;
         public stackElement<T> next;
+        //public stackElement<T> prev;
     }
     public class myStack<T>
     {
@@ -36,6 +37,7 @@ namespace article3
             if (lenght == 0)
             {
                 newEl.next = null;
+                //newEl.prev = null;
                 first = newEl;
             }
             else
@@ -44,6 +46,65 @@ namespace article3
             }
             last = newEl;
             lenght++;
+        }
+        public T pop()
+        {
+            T res = default(T);
+            if (lenght > 0)
+            {
+                res = last.value;
+                last = last.next;
+                lenght--;
+            }
+            return res;
+        }
+    }
+    public class fifoElement<T>
+    {
+        public T value;
+        public fifoElement<T> prev;
+    }
+    public class myFIFO<T>
+    {
+        private int lenght;
+        private fifoElement<T> first;
+        private fifoElement<T> last;
+        public myFIFO()
+        {
+            lenght = 0;
+            first = null;
+            last = null;
+        }
+        public int Lenght
+        {
+            get {return lenght;}
+        }
+        public void add(T newElement)
+        {
+            fifoElement<T> newEl = new fifoElement<T>();
+            newEl.value = newElement;
+            if(lenght == 0)
+            {
+                newEl.prev = null;
+                first = newEl; 
+            }
+            else
+            {
+                last.prev = newEl;
+            }
+            lenght++;
+            last = newEl;
+        }
+        public T remove()
+        {
+            T res = default(T);
+            if(lenght > 0)
+            {
+                res = first.value;
+                first = first.prev;
+                lenght--;
+            }
+            return res;
         }
     }
 }
